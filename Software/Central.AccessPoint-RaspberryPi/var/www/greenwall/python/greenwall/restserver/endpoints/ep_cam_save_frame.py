c6e9453059940217966fa88f699044fbd9f685c0
dallaszkorben <dallaszkorben@gmail.com>
2022-07-29 23:50:48 +0200

    Capture images from the Frame => 1 image/minute The single Capture Page refreshes

--- Software/Central.AccessPoint-RaspberryPi/var/www/greenwall/python/greenwall/restserver/endpoints/ep_cam_save_frame.py
+++ Software/Central.AccessPoint-RaspberryPi/var/www/greenwall/python/greenwall/restserver/endpoints/ep_cam_save_frame.py
@@ -73,21 +73,26 @@ class EPCamSaveFrame(EP):
                 EPCamSaveFrame.ATTR_IMAGE, image ))
 
         dateString = datetime.now().astimezone().isoformat()
-
-
-
         absoluteRootPath = self.web_gadget.absoluteRootPath
-        webCamFrameFolder = self.web_gadget.webCamFrameFolder
 
-        fileName = f'{dateString}.jpg'
-        # constract the path /var/www/greenwall/cam-frame/5
-        path = os.path.join(absoluteRootPath, webCamFrameFolder, camId)
-        fileNamePath = os.path.join(path, fileName)
+        # constract the frame-file path to /var/www/greenwall/cam-frame/5
+        webCamFrameFolder = self.web_gadget.webCamFrameFolder
+        frameFileName = f'{dateString}.jpg'
+        framePath = os.path.join(absoluteRootPath, webCamFrameFolder, camId)
+        frameFileNamePath = os.path.join(framePath, frameFileName)
+
+        # constract the capture-file path to /var/www/greenwall/capture
+        webCamCaptureFolder = self.web_gadget.webCamCaptureFolder
+        captureFileName = self.web_gadget.webCamCaptureFile.format(camId)
+        capturePath = os.path.join(absoluteRootPath, webCamCaptureFolder)
+        captureFileNamePath = os.path.join(capturePath, captureFileName)
+
 
         if image:
 
-            # Create the path if it dees not exist
-            Path(path).mkdir(parents=True, exist_ok=True)
+            #
+            # Save image as FRAME
+            #
 
             img = Image.open(image)
 
@@ -95,20 +100,37 @@ class EPCamSaveFrame(EP):
                 # add timestamp to the image
                 draw = ImageDraw.Draw(img)
             except Exception as e:
-                logging.error("      {0} FRAME was NOT saved. ImageDraw FAILE\n{1}".format(fileNamePath,e))
+                logging.error("      {0} FRAME/CAPTURE was NOT saved. ImageDraw FAILE\n{1}".format(frameFileNamePath,e))
                 return output_json( {'result': 'ERROR'}, EP.CODE_BAD_REQUEST)
 
             font = ImageFont.truetype("DejaVuSans.ttf", 32)
             draw.text((0,20), dateString, (100,100,100), font=font)
 
-            img.save(fileNamePath)
+            # ---
 
-            logging.debug("      {0} FRAME was saved".format(fileNamePath))
+            # Create the path for FRAME if it dees not exist
+            Path(framePath).mkdir(parents=True, exist_ok=True)
+
+            # Save FRAME
+            img.save(frameFileNamePath)
+            logging.debug("      {0} FRAME was saved".format(frameFileNamePath))
+
+            # ---
+
+            # Create the path for CAPTURE if it dees not exist
+            Path(capturePath).mkdir(parents=True, exist_ok=True)
+
+            # Save CAPTURE
+            img.save(captureFileNamePath)
+            logging.debug("      {0} CAPTURE was saved".format(frameFileNamePath))
+
+            # ---
+
             return output_json( {'result': 'OK'}, EP.CODE_OK)
 
         else:
 
-            logging.error( "      !!! No {0} was saved as there was NO image sent) !!!".format(fileNamePath))
+            logging.error( "      !!! No {0} was saved as there was NO image sent) !!!".format(frameFileNamePath))
             return output_json( {'result': 'ERROR'}, EP.CODE_BAD_REQUEST)
 
 
